695268c832ba676b54ee5046b1f97d36f45f61a1
dimchy <dipuzyrev@gmail.com>
2026-07-28 09:38:40 +0200

    fix gitignore, restore lost test file

---
+++ apps/extension/src/tests/test-projects/vue-i18n/src/unit-tests/extraction-in-script.js
@@ -0,0 +1,13 @@
+export const osTitles = {
+  win: 'Windows',
+  mac: 'Mac',
+  lnx: 'Linux',
+}
+
+const userName = "Dmitrii"
+const templateLiteral = `Hi, ${userName}`
+
+const nestedQuotes1 = "I'm her'e ye's \"bro\"."
+const nestedQuotes2 = 'Im here,  bro"'
+
+const stringInsideTemplate = `hello ${'hi'} yo`
